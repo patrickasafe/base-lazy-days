052da59287dfa1db738345c410eb623cfa2937d5
Patrick Asafe <patrickasafe@gmail.com>
2022-02-27 15:08:36 -0300

    ADD usePrefetchTreatments

--- client/src/components/treatments/hooks/useTreatments.ts
+++ client/src/components/treatments/hooks/useTreatments.ts
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
@@ -14,4 +14,9 @@ export function useTreatments(): Treatment[] {
   const fallback = [];
   const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
   return data;
+}
+
+export const usePrefetchTreatments = (): void => {
+  const queryClient = useQueryClient();
+  queryClient.prefetchInfiniteQuery(queryKeys.treatments, getTreatments)
 }
