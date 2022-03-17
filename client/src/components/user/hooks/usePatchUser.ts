bb2f27ecd0a75500e9dcd60bbec698b6b45aa2e7
patrickasafe <patrickasafe@gmail.com>
2022-03-17 15:18:09 -0300

    EDIT usePatchUser (optimistic update)

--- client/src/components/user/hooks/usePatchUser.ts
+++ client/src/components/user/hooks/usePatchUser.ts
@@ -1,8 +1,9 @@
 import jsonpatch from 'fast-json-patch';
-import { UseMutateFunction, useMutation } from 'react-query';
+import { UseMutateFunction, useMutation, useQueryClient } from 'react-query';
 
 import type { User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
+import { queryKeys } from '../../../react-query/constants';
 import { useCustomToast } from '../../app/hooks/useCustomToast';
 import { useUser } from './useUser';
 
@@ -35,10 +36,27 @@ export function usePatchUser(): UseMutateFunction<
 > {
   const { user, updateUser } = useUser();
   const toast = useCustomToast();
+  const queryClient = useQueryClient();
 
   const { mutate: patchUser } = useMutation(
     (newUserData: User) => patchUserOnServer(newUserData, user),
     {
+      onMutate: async (newData: User | null) => {
+        queryClient.cancelQueries(queryKeys.user);
+        const previousUserData: User = queryClient.getQueryData(queryKeys.user);
+        updateUser(newData);
+        return { previousUserData };
+      },
+      onError: (error, newData, context) => {
+        if (context.previousUserData) {
+          updateUser(context.previousUserData);
+          toast({
+            title: 'Update failed; restoring previous values',
+            status: 'warning',
+          });
+        }
+      },
+
       onSuccess: (userData: User | null) => {
         if (user) {
           updateUser(userData);
@@ -47,6 +65,9 @@ export function usePatchUser(): UseMutateFunction<
             status: 'success',
           });
         }
+      },
+      onSettled: () => {
+        queryClient.invalidateQueries(queryKeys.user);
       },
     },
   );
