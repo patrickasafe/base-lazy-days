78bd4a7e69c3f9a1839a5862379d2c5cee02c3d3
patrickasafe <patrickasafe@gmail.com>
2022-03-23 15:34:04 -0300

    ADD AllStaff tests

--- client/src/components/appointments/tests/Calendar.test.tsx
+++ client/src/components/appointments/tests/Calendar.test.tsx
@@ -2,15 +2,9 @@ import { screen } from '@testing-library/react';
 import { rest } from 'msw';
 
 import { server } from '../../../mocks/server';
-// import { defaultQueryClientOptions } from '../../../react-query/queryClient';
 import { renderWithClient, renderWithQueryClient } from '../../../test-utils';
 import { Calendar } from '../Calendar';
 
-// mocking useUser to mimic a logged-in user
-// jest.mock('../../user/hooks/useUser', () => ({
-//   __esModule: true,
-//   useUser: () => ({ user: mockUser }),
-// }));
 
 test('Reserve appointment error', async () => {
   // (re)set handler to return a 500 error for appointments
--- client/src/components/staff/tests/AllStaff.test.tsx
+++ client/src/components/staff/tests/AllStaff.test.tsx
@@ -1,7 +1,9 @@
-import { screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { rest } from 'msw';
+import { QueryClientProvider, setLogger } from 'react-query';
 
 import { server } from '../../../mocks/server';
+import { generateQueryClient } from 'react-query/queryClient';
 import { renderWithQueryClient } from '../../../test-utils';
 import { AllStaff } from '../AllStaff';
 
@@ -21,4 +23,30 @@ test('handles query error', async () => {
       return res(ctx.status(500));
     }),
   );
+  // suppress errors
+  setLogger({
+    log: console.log,
+    warn: console.warn,
+    error: () => {
+      // no printing
+    },
+  })
+  
+  // set up query client with retries set to false
+  const queryClient = generateQueryClient();
+  const options  = queryClient.getDefaultOptions();
+  options.queries = { ...options.queries, retry: false };
+  queryClient.setDefaultOptions(options);
+
+  // render wrapped with provider
+  render(
+    <QueryClientProvider client={queryClient}>
+      <AllStaff />
+    </QueryClientProvider>
+  );
+
+  // check for the toast alert
+  const alertToast = await screen.findByRole('alert');
+  expect(alertToast).toHaveTextContent('Request failed with status code 500')
 });
+
